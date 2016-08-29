53c283b0be2d45dd667b8347ce39e2abfe3a89cf
Petro Prykhodko <p.prykhodko@bank4you.lu>
2016-08-29 11:35:00 +0300

    Initial commit

---
+++ MailTester/MailTester/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Mail;
+using System.Net;
+using System.IO;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var path = @"D:\mail\taxfree.html";
+
+            var client = new SmtpClient("mailtrap.io", 2525)
+            {
+                Credentials = new NetworkCredential("fa6098eec55199", "f6554503eced6f"),
+                EnableSsl = true
+            };
+
+            var from = "no-reply@taxfree.eu";
+            var to = "ayavorskiy.runme@previews.emailonacid.com";
+            //var to = "to@example.com";
+            var subject = "TaxFree";
+
+            var message = "";
+
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.UTF8))
+            {
+                var text = sr.ReadToEnd();
+                message = text;
+
+                Console.WriteLine(text);
+            }
+
+            MailMessage mail = new MailMessage(from, to, subject, message);
+            mail.IsBodyHtml = true;
+
+            client.Send(mail);
+            Console.WriteLine("===================================================Sent====================================================================");
+            Console.ReadLine();
+        }
+    }
+}
